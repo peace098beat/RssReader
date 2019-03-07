acb993d09c3bc41bf5aa729bafefd77a7322143f
peace098beat <peace098beat.t@gmail.com>
2019-03-07 19:06:37 +0900

    FirstCommit

--- RssReader/Form1.Designer.cs
+++ RssReader/Form1.Designer.cs
@@ -28,13 +28,60 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(155, 12);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(239, 237);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(13, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.textBox1.Font = new System.Drawing.Font("Myrica M", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.textBox1.ForeColor = System.Drawing.Color.YellowGreen;
+            this.textBox1.Location = new System.Drawing.Point(12, 53);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(136, 196);
+            this.textBox1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(406, 262);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
 
--- RssReader/Form1.cs
+++ RssReader/Form1.cs
@@ -1,12 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.ServiceModel.Syndication;
 using System.Windows.Forms;
+using System.Xml;
 
 namespace RssReader
 {
@@ -15,6 +10,42 @@ namespace RssReader
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            var sw = System.Diagnostics.Stopwatch.StartNew();
+
+            textBox1.Text += $"start"+Environment.NewLine;
+
+
+            string url = @"https://www.google.co.jp/alerts/feeds/09047520966360389555/18173224082898862477";
+
+            using (XmlReader rdr = XmlReader.Create(url))
+            {
+                SyndicationFeed feed = SyndicationFeed.Load(rdr);
+
+                richTextBox1.Text = "";
+
+
+                foreach (SyndicationItem item in feed.Items)
+                {
+                    TextSyndicationContent c = (TextSyndicationContent)item.Content;
+
+
+                    richTextBox1.Text += $"{c.Text}" + Environment.NewLine;
+
+
+                    //Console.WriteLine("item Title:" + item.Title.Text);
+                    //Console.WriteLine("item Title:" + item.Content.Type);
+                    //Console.WriteLine("item Title:" + c.Text);
+
+                    //Console.WriteLine("link:" + (item.Links.Count > 0
+                    //                ? item.Links[0].Uri.AbsolutePath : ""));
+                }
+            }
+
+            textBox1.Text += $"{sw.Elapsed}" + Environment.NewLine;
         }
     }
 }
